a9f64db9c2155f908e5dc2ebf11ef532bfeaf282
Angelo Charles G. Sia <acgsia@gmail.com>
2015-02-28 11:24:13 +0800

    Step K. Update field myaddress_id

--- practice/midterm_test/Sia_Angelo_Charles_midterms/ourspace/views/mycomment/_form.php
+++ practice/midterm_test/Sia_Angelo_Charles_midterms/ourspace/views/mycomment/_form.php
@@ -1,7 +1,9 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use app\models\MyAddress;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\MyComment */
@@ -14,7 +16,10 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'myaddress_id')->textInput() ?>
+	<?= $form->field($model, 'myaddress_id')-> dropDownList(
+		ArrayHelper::map(MyAddress::find()->all(),'id','lastname'),
+		['prompt'=>'Select Last Name']
+	) ?>
 
     <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
 
