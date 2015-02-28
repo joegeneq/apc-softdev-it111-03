04c68b99184be52cf45fcd3728fadcb54dc3bcfb
Julian Acuna <julian1994acuna@gmail.com>
2015-02-28 10:10:46 +0800

    update

--- practice/agsia/basic/models/CitySearch.php
+++ practice/agsia/basic/models/CitySearch.php
@@ -5,7 +5,7 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\City;
+use app\models\Province;
 
 /**
  * CitySearch represents the model behind the search form about `app\models\City`.
@@ -18,8 +18,8 @@ class CitySearch extends City
     public function rules()
     {
         return [
-            [['id', 'province_id'], 'integer'],
-            [['city_code', 'city_description'], 'safe'],
+            [['id',], 'integer'],
+            [['city_code', 'province_description', 'province_id'], 'safe'],
         ];
     }
 
