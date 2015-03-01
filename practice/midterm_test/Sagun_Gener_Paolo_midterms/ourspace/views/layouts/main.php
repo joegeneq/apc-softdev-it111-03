04fc01727cce204a62fae4866a9f0ca071488d72
Gener Paolo Sagun <sagungenerpaolo@gmail.com>
2015-03-01 17:12:18 +0800

    Modified main.php

--- practice/midterm_test/Sagun_Gener_Paolo_midterms/ourspace/views/layouts/main.php
+++ practice/midterm_test/Sagun_Gener_Paolo_midterms/ourspace/views/layouts/main.php
@@ -13,6 +13,7 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,7 +21,7 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body bgcolor="#E6E6FA">
+<body>
 
 <?php $this->beginBody() ?>
     <div class="wrap">
