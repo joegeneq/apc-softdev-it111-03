c556519b5d718d403b91ff5055003624793b216f
Angelo Sia <acgsia@gmail.com>
2015-03-07 19:04:35 +0800

    modify footer css

--- application/mbc/frontend/views/layouts/footer.php
+++ application/mbc/frontend/views/layouts/footer.php
@@ -1,6 +1,6 @@
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; <?= date('Y') ?> MBC-RTS ALL RIGHTS RESERVED</p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p> &copy; <?= date('Y') ?> MBC-RTS ALL RIGHTS RESERVED</p>
+        <!--<p class="pull-right"><?= Yii::powered() ?></p>-->
         </div>
     </footer>
