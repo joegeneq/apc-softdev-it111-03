a04de0e8d04f561cd806a269a57a412378033689
Gener Paolo Sagun <sagungenerpaolo@gmail.com>
2015-03-08 15:12:28 +0800

    edit about.php and site.css

--- application/mbc/frontend/views/site/about.php
+++ application/mbc/frontend/views/site/about.php
@@ -5,13 +5,14 @@ use yii\helpers\Html;
 $this->title = 'Muntinlupa Baptist Church Reports Tracking System';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-   <img alt="MuntinlupaBaptistChurch_logo1_zps457d76c8.jpg" width="648" height="700" 
-	src="http://i1284.photobucket.com/albums/a561/cbsamson16/MuntinlupaBaptistChurch_logo1_zps457d76c8.jpg"> </img>
+<div id="aboutImage" class="site-about">
+    <h1 font-color="green"><?= Html::encode($this->title) ?></h1>
 
+	<h2>About Us</h2>
     <p>This is the About page. You may modify the following file to customize its content:</p>
 
     <code><?= __FILE__ ?></code>
 </div>
+
+
