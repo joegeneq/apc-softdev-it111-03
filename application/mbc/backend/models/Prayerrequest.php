2010f6ff7de2746149d2e607644af2e23d346325
Julian Acuna <julian1994acuna@gmail.com>
2015-03-11 10:55:44 +0800

    created prayerrequest backend

---
+++ application/mbc/backend/models/Prayerrequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "prayerrequest".
+ *
+ * @property integer $id
+ * @property integer $prayerrequest_code
+ * @property string $prayerrequest_type
+ * @property string $prayerrequest_description
+ *
+ * @property FilteredPrayerrequest[] $filteredPrayerrequests
+ * @property MemberGivesPrayerrequest[] $memberGivesPrayerrequests
+ */
+class Prayerrequest extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'prayerrequest';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['prayerrequest_code'], 'integer'],
+            [['prayerrequest_type'], 'string', 'max' => 45],
+            [['prayerrequest_description'], 'string', 'max' => 100]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'prayerrequest_code' => 'Prayerrequest Code',
+            'prayerrequest_type' => 'Prayerrequest Type',
+            'prayerrequest_description' => 'Prayerrequest Description',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFilteredPrayerrequests()
+    {
+        return $this->hasMany(FilteredPrayerrequest::className(), ['prayerrequest_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMemberGivesPrayerrequests()
+    {
+        return $this->hasMany(MemberGivesPrayerrequest::className(), ['prayerrequest_id' => 'id']);
+    }
+}
---
+++ application/mbc/backend/views/prayerrequest/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\PrayerRequest */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="prayer-request-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'prayerrequest_code')->textInput() ?>
+
+    <?= $form->field($model, 'prayerrequest_type')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'prayerrequest_description')->textInput(['maxlength' => 100]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
