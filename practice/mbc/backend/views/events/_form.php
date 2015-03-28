bd963a2f227da35d0f12f7a27a525c5c5ed868f9
Angelo Sia <acgsia@gmail.com>
2015-03-28 19:51:04 +0800

    final project

---
+++ practice/mbc/backend/views/events/_form.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Events */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="events-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+   <!-- <?= $form->field($model, 'events_date')->textInput() ?> -->
+
+   <?= $form->field($model, 'events_date')->widget(
+    DatePicker::className(), [
+        // inline too, not bad
+        'inline' => false, 
+        'clientOptions' => [
+            'autoclose' => true,
+            'startDate' => '+0d',
+            'format' => 'yy-mm-dd' 
+        ]
+	]);?>
+
+    <?= $form->field($model, 'events_location')->textInput(['maxlength' => 100]) ?>
+
+    <!-- <?= $form->field($model, 'events_prioritylevel')->textInput() ?> -->
+
+    <?= $form->field($model, 'events_prioritylevel')->textInput() 
+
+    ->dropDownList(
+            $items = 
+	            [
+		            '1'=>'High',
+		            '2'=>'Normal'
+		            
+
+	            ], // Flat array ('id'=>'label')
+            ['prompt'=>'Select Priority Level']    // options
+        );
+     ?>
+
+    <?= $form->field($model, 'no_of_attendees')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ practice/mbc/backend/views/prayerrequest/_form.php
@@ -0,0 +1,60 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\PrayerRequest */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="prayer-request-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'prayerrequest_code')->textInput() 
+
+    ->dropDownList(
+            $items = 
+	            [
+		            '1'=>'Public',
+		            '2'=>'Private'
+		            
+
+	            ], // Flat array ('id'=>'label')
+            ['prompt'=>'Select PrivacyType']    // options
+        );
+     ?>
+
+    <?=  $form->field($model, 'prayerrequest_type')
+        ->dropDownList(
+            $items = 
+	            [
+		            'Health'=>'Health',
+		            'Career'=>'Career',
+		            'Studies'=>'Studies',
+		            'Pregnancy'=>'Pregnancy',
+		            'Family'=>'Family',
+		            'Relationship'=>'Relationship',
+		            'Travelling'=>'Travelling',
+		            'Spiritual Growth'=>'Spiritual Growth',
+		            'Evangelistic'=>'Evangelistic',
+		            'Financial'=>'Financial',
+		            'Reconciliation'=>'Reconciliation',
+		            'Others'=>'Others'
+
+	            ], // Flat array ('id'=>'label')
+            ['prompt'=>'Select PrayerType']    // options
+        );
+     ?>
+
+
+    <?= $form->field($model, 'prayerrequest_description')->textInput(['maxlength' => 100]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
