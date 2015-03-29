ab3480b04df850fb1aec0ea566d29b908408fd55
Angelo Sia <acgsia@gmail.com>
2015-03-29 18:19:58 +0800

    created models/User.php

---
+++ practice/mbc/backend/models/User.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $auth_key
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string $email
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $user_lastname
+ * @property string $user_firstname
+ * @property string $user_contactno
+ * @property string $user_homeadd
+ * @property string $user_emailadd
+ * @property string $user_actministry
+ * @property string $user_attendance
+ * @property string $user_type
+ *
+ * @property MemberGivesPrayerrequest[] $memberGivesPrayerrequests
+ * @property MemberHasEvents[] $memberHasEvents
+ * @property Tithe[] $tithes
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'auth_key', 'password_hash', 'email', 'created_at', 'updated_at'], 'required'],
+            [['status', 'created_at', 'updated_at'], 'integer'],
+            [['username', 'password_hash', 'password_reset_token', 'email'], 'string', 'max' => 255],
+            [['auth_key'], 'string', 'max' => 32],
+            [['user_lastname', 'user_firstname', 'user_contactno', 'user_homeadd', 'user_emailadd', 'user_actministry', 'user_attendance'], 'string', 'max' => 45],
+            [['user_type'], 'string', 'max' => 20]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'auth_key' => 'Auth Key',
+            'password_hash' => 'Password Hash',
+            'password_reset_token' => 'Password Reset Token',
+            'email' => 'Email',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'user_lastname' => 'User Lastname',
+            'user_firstname' => 'User Firstname',
+            'user_contactno' => 'User Contactno',
+            'user_homeadd' => 'User Homeadd',
+            'user_emailadd' => 'User Emailadd',
+            'user_actministry' => 'User Actministry',
+            'user_attendance' => 'User Attendance',
+            'user_type' => 'User Type',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMemberGivesPrayerrequests()
+    {
+        return $this->hasMany(MemberGivesPrayerrequest::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMemberHasEvents()
+    {
+        return $this->hasMany(MemberHasEvents::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTithes()
+    {
+        return $this->hasMany(Tithe::className(), ['user_id' => 'id']);
+    }
+}
