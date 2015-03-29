9b2ab1520a0b26e5a3127fd5328359f7c90d2c7c
Angelo Sia <acgsia@gmail.com>
2015-03-30 03:07:47 +0800

    updated sign up reference for backend

--- practice/mbc/frontend/views/site/signup.php
+++ practice/mbc/frontend/views/site/signup.php
@@ -17,9 +17,16 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+                <?= $form->field($model, 'user_lastname') ?>
+                <?= $form->field($model, 'user_firstname') ?>
+                <?= $form->field($model, 'user_contactno') ?>
+                <?= $form->field($model, 'user_homeadd') ?>
+                <?= $form->field($model, 'user_actministry') ?>
+                <?= $form->field($model, 'user_attendance') ?>
                 <?= $form->field($model, 'username') ?>
                 <?= $form->field($model, 'email') ?>
                 <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'user_type') ?>
                 <div class="form-group">
                     <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
                 </div>
