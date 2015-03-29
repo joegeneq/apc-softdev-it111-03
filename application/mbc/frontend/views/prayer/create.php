8528191c39f0e0fbd7c1bdb0cc05ee72645e9e7f
Julian Acuna <julian1994acuna@gmail.com>
2015-03-29 20:52:19 +0800

    regenerated CRUD for prayer in frontend and backend

---
+++ application/mbc/frontend/views/prayer/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Prayer */
+
+$this->title = 'Create Prayer';
+$this->params['breadcrumbs'][] = ['label' => 'Prayers', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="prayer-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
