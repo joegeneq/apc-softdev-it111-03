091a9e3e5a53a0ccd5ff3722a7e7bf72c8beab76
Angelo Sia <acgsia@gmail.com>
2015-03-30 18:08:23 +0800

    added TitheController.php

---
+++ practice/mbc/backend/controllers/TitheController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use backend\models\Tithe;
+use backend\models\TitheSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * TitheController implements the CRUD actions for Tithe model.
+ */
+class TitheController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Tithe models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new TitheSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Tithe model.
+     * @param integer $id
+     * @param integer $user_id
+     * @return mixed
+     */
+    public function actionView($id, $user_id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id, $user_id),
+        ]);
+    }
+
+    /**
+     * Creates a new Tithe model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Tithe();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id, 'user_id' => $model->user_id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Tithe model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @param integer $user_id
+     * @return mixed
+     */
+    public function actionUpdate($id, $user_id)
+    {
+        $model = $this->findModel($id, $user_id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id, 'user_id' => $model->user_id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Tithe model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @param integer $user_id
+     * @return mixed
+     */
+    public function actionDelete($id, $user_id)
+    {
+        $this->findModel($id, $user_id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Tithe model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @param integer $user_id
+     * @return Tithe the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id, $user_id)
+    {
+        if (($model = Tithe::findOne(['id' => $id, 'user_id' => $user_id])) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
