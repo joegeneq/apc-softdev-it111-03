53de54dcb05995351e5f259e65c7528fb70b78f8
Gener Paolo Sagun <sagungenerpaolo@gmail.com>
2015-03-30 19:59:35 +0800

    Generated model and crud in events table

---
+++ application/mbc/backend/models/Events.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "events".
+ *
+ * @property integer $id
+ * @property string $events_date
+ * @property string $events_location
+ * @property string $events_prioritylevel
+ * @property string $event_desc
+ * @property integer $no_of_attendees
+ *
+ * @property UserHasEvents[] $userHasEvents
+ */
+class Events extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'events';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['events_date'], 'safe'],
+            [['no_of_attendees'], 'integer'],
+            [['events_location'], 'string', 'max' => 100],
+            [['events_prioritylevel'], 'string', 'max' => 10],
+            [['event_desc'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'events_date' => 'Events Date',
+            'events_location' => 'Events Location',
+            'events_prioritylevel' => 'Events Prioritylevel',
+            'event_desc' => 'Event Desc',
+            'no_of_attendees' => 'No Of Attendees',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserHasEvents()
+    {
+        return $this->hasMany(UserHasEvents::className(), ['events_id' => 'id']);
+    }
+}
