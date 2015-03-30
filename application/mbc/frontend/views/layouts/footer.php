04442df23b86014a15a7789f05ab44e6d6d6c436
Angelo Sia <acgsia@gmail.com>
2015-03-30 18:48:43 +0800

    deleted old project

--- application/mbc/frontend/views/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-    <footer class="footer">
-        <div class="container">
-        <p> &copy; <?= date('Y') ?> MBC-RTS ALL RIGHTS RESERVED</p>
-        <!--<p class="pull-right"><?= Yii::powered() ?></p>-->
-        </div>
-    </footer>
