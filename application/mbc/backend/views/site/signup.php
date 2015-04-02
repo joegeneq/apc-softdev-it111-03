e8eeccd7040cf8a54496e88ed71148a33482ed7d
Angelo Sia <acgsia@gmail.com>
2015-04-02 21:35:46 +0800

    Rename "sign up" to "create a User"

--- application/mbc/backend/views/site/signup.php
+++ application/mbc/backend/views/site/signup.php
@@ -7,13 +7,13 @@ use yii\helpers\ArrayHelper;
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \frontend\models\SignupForm */
 
-$this->title = 'Signup';
+$this->title = 'Add User';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to signup:</p>
+    <p>Please fill out the following fields to add user:</p>
 
     <div class="row">
         <div class="col-lg-5">
