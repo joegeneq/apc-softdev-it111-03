6b94c57f35419af3a81375edcc9072b94f9aa9f2
Angelo Sia <acgsia@gmail.com>
2015-04-04 22:37:19 +0800

    Fix form order

--- application/mbc/frontend/views/prayer/_form.php
+++ application/mbc/frontend/views/prayer/_form.php
@@ -15,32 +15,8 @@ use dosamigos\datepicker\DatePicker;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'prayer_desc')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'prayer_type')->textInput(['maxlength' => 45])
-    ->dropDownList(
-            $items = 
-                [
-                    'Career'=>'Career',
-                    'Evangelistic'=>'Evangelistic',
-                    'Family'=>'Family',
-                    'Financial'=>'Financial',
-                    'Health'=>'Health',
-                    'Pregnancy'=>'Pregnancy',
-                    'Reconciliation'=>'Reconciliation',
-                    'Relationship'=>'Relationship',
-                    'Spiritual Growth'=>'Spiritual Growth',
-                    'Studies'=>'Studies',
-                    'Travelling'=>'Travelling',
-                    'Others'=>'Others'
-
-                ], // Flat array ('id'=>'label')
-            ['prompt'=>'Select PrayerType']    // options
-        );
- ?>
-
-    <?= $form->field($model, 'prayer_code')->textInput(['maxlength' => 10])
-    ->dropDownList(
+        <?= $form->field($model, 'prayer_code')->textInput(['maxlength' => 10])
+        ->dropDownList(
             $items = 
                 [
                     'Public'=>'Public',
@@ -51,18 +27,42 @@ use dosamigos\datepicker\DatePicker;
         );
         ?>
 
+        <?= $form->field($model, 'prayer_type')->textInput(['maxlength' => 45])
+        ->dropDownList(
+                $items = 
+                    [
+                        'Career'=>'Career',
+                        'Evangelistic'=>'Evangelistic',
+                        'Family'=>'Family',
+                        'Financial'=>'Financial',
+                        'Health'=>'Health',
+                        'Pregnancy'=>'Pregnancy',
+                        'Reconciliation'=>'Reconciliation',
+                        'Relationship'=>'Relationship',
+                        'Spiritual Growth'=>'Spiritual Growth',
+                        'Studies'=>'Studies',
+                        'Travelling'=>'Travelling',
+                        'Others'=>'Others'
+
+                    ], // Flat array ('id'=>'label')
+                ['prompt'=>'Select PrayerType']    // options
+            );
+        ?>
+
+        <?= $form->field($model, 'prayer_desc')->textInput(['maxlength' => 100]) ?>
+
   <!--   <?= $form->field($model, 'prayer_schedule')->textInput() ?> -->
-    <?= $form->field($model, 'prayer_schedule')->widget(
-    DatePicker::className(), [
-        // inline too, not bad
-         'inline' => false, 
-         // modify template for custom rendering
-       // 'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
-        'clientOptions' => [
-            'autoclose' => true,
-            'format' => 'yyyy-mm-dd'
-        ]
-]);?>
+        <?= $form->field($model, 'prayer_schedule')->widget(
+        DatePicker::className(), [
+            // inline too, not bad
+             'inline' => false, 
+             // modify template for custom rendering
+           // 'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd'
+            ]
+        ]);?>
 
     <?= $form->field($model, 'user_name')->textInput(['value'=>Yii::$app->user->identity->user_name, 'readOnly'=>true]) ?>
 
